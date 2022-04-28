6a0cafed1243bedfd2eb9f69d43af853c82aace4
Jack Sweeny <jsweeny344@gmail.com>
2022-04-27 21:50:28 -0700

    Newest Commit

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,16 +1,20 @@
-
+import '../styles/InputForm.css'
 import {db} from "../firebase";
 import {ref, onValue, query, getDatabase, get, child, on, equalTo, DataSnapshot, push } from "firebase/database";
 import { useState, useEffect } from "react";
+import arrayShuffle from "array-shuffle";
+import Select from 'react-select';
 
 
 const InputForm = () => {
 
-    const [previousCharacter, setPreviousCharacter] = useState();
-    const [roll, setRoll] = useState();
+    const [previousCharacter, setPreviousCharacter] = useState(0);
+    const [roll, setRoll] = useState(0);
     // const [nextCharacter, setNextCharacter] = useState('');
     const [nextCharacterName, setNextCharacterName] = useState('')
     const [charSelectList, setCharSelectList] = useState([]);
+    const [currentList, setCurrentList] = useState([]);
+    const [disabledCharacters, setDisabledCharacters] = useState([])
 
 
     useEffect(() => {
@@ -20,55 +24,142 @@ const InputForm = () => {
                 // const data = ;
                 // const dataArry =;
                 
-                setCharSelectList([snapshot.val()][0])
+                setCharSelectList([snapshot.val()][0]);
+                
                 return charSelectList;
                
             } else{
                 console.log('no data')
             }
         })
-    },[nextCharacterName])
+    },[])
 
+    
+    
+    
     
     const handleSubmit = (e) =>{
         e.preventDefault();
+        if (Number(roll) + Number(previousCharacter) > charSelectList.length) {
+            setNextCharacterName(charSelectList[charSelectList.length -1].name);
+            // console.log(nextCharacterName)
+            // console.log(charSelectList[charSelectList.length -1])
+        } else{
         setNextCharacterName(charSelectList[Number(roll) + Number(previousCharacter)].name)
+        setPreviousCharacter(charSelectList[Number(roll) + Number(previousCharacter)].index)
+        console.log(previousCharacter);
+        // console.log('missed')
+        }
+        
     }
 
     const handleRoll = (e) =>{
         setRoll(e.target.value)
 
+        // console.log('rolled a ' + roll)
     }   
-   
-    const newData = []
-    
+
     const handleChange = (e) =>{
-        setPreviousCharacter(e.target.value);
+        setPreviousCharacter(Number(e.target.value));
         console.log(previousCharacter)
     }
 
 
+
+    const reverseList = () =>{
+        // e.preventDefault();
+        setCharSelectList(charSelectList.reverse())
+        setPreviousCharacter()
+        // console.log(charSelectList)
+        // console.log(previousCharacter)
+        // return charSelectList;
+    }
+
+    const shuffleList = () => {
+
+    //   const tempList = arrayShuffle(charSelectList)  
+      setCharSelectList(arrayShuffle(charSelectList))
+    //   console.log(charSelectList);
+    }
+
+    
+
+   const tempArray = []; 
+
+   const handleCharDisable = (e) => {
+   console.log(e.target.value);
+   const value = e.target.value;
+
+   setDisabledCharacters(disabledCharacters => [...disabledCharacters, value]);
+    console.log(disabledCharacters);
+//    tempArray.push(value)
+//    console.log(tempArray);
+//    setDisabledCharacters(tempArray)
+//    return tempArray;
+   }
+
+   const disableList = () => {
+    setCharSelectList(charSelectList.filter((char) => {
+        if(char.id == disabledCharacters) {
+            char.splice()
+        } 
+    }))
+}
+  
+
     return (
         <>
             <form  onSubmit={ handleSubmit }className="inputForm">
-                <label className="inputLabel">Last Character</label>
-                <select name="charSelect" onChange={handleChange}  >
-                   {  charSelectList.map((char) => (
-                    <option value={char.id}  key={char.id}>{ char.name }</option>
+                <label className="inputLabel">Last Character: </label>
+                <select name="charSelect" className='select-input' onChange={handleChange}  >
+                   {  charSelectList.map((char, index) => (
+                    <option value={index}  key={char.id}>{ char.name }</option>
                     )) }
                 </select>
-                <p>{previousCharacter}</p>
+                {/* <p>{previousCharacter }</p> */}
                 <br />
-                <label className="inputLabel"> Roll </label>
-                <input type="number" name="roll" onChange={handleRoll} />
+                <label className="inputLabel"> Roll: </label>
+                <input type="number" name="roll" className='roll-input' onChange={handleRoll} />
                 <p>{roll}</p>
                 <br />
-                <button> </button>
-                <button type="submit">Get Next Character</button>
-
+                <button type="submit" className='next-character-button'>Get Next Character</button>
+                
+               <select name="charDisableComp" id="" onChange={ handleCharDisable }>
+                   { charSelectList.map((char) => (
+                       <option value={ char.id } key={char.id}> { char.name }</option>
+                   )) }
+               </select>
                 <h3>{ 'Next Character: ' + nextCharacterName } </h3>
-                <h3>{}</h3>
+
+                
             </form> 
+            <div className="btn-wrapper">
+            <button className='btn-function' onClick={reverseList}> Reverse</button>
+            <button className='btn-function' onClick={shuffleList}> Shuffle </button>
+            <button className='btn-function' onClick={disableList}> Disable Characters</button>
+            </div>
+            
+            <div className="list-wrapper">
+                <div className="list-1">
+                    <h3> Character List </h3>
+                    { charSelectList.map((char, index) => (
+                            <p value={ index } key={char.id}> { char.name  }  { index } </p>
+                        )) }
+                </div>
+                
+                <div className="list-2">
+                    <h3> Characters To Be Removed </h3>
+                    {
+                    disabledCharacters.map((char) =>(
+                        <p> { charSelectList[char].name} </p>
+                    ))
+                    }
+                </div>
+               
+            
+            </div>
+           
+            
              
            
               
--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import {getDatabase } from "firebase/database"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCbgn3KTsAoiNCecrWeIxJyA9nG5uH8v5k",
-  authDomain: "smash-dice-game.firebaseapp.com",
-  databaseURL: "https://smash-dice-game-default-rtdb.firebaseio.com",
-  projectId: "smash-dice-game",
-  storageBucket: "smash-dice-game.appspot.com",
-  messagingSenderId: "762148609845",
-  appId: "1:762148609845:web:f04769df28e099a0ea767c"
+  apiKey: "AIzaSyBx1W4a6IgGcWT8j0yZur84-jlkSIFj_Wk",
+  authDomain: "fir-practice-c292d.firebaseapp.com",
+  databaseURL: "https://fir-practice-c292d-default-rtdb.firebaseio.com",
+  projectId: "fir-practice-c292d",
+  storageBucket: "fir-practice-c292d.appspot.com",
+  messagingSenderId: "658290737824",
+  appId: "1:658290737824:web:056538ed2839197b5c7fc5"
 };
 
 // Initialize Firebase
